b04d3bc1dc21c40d1fcd519cfa3a51f0cbc2cfaa
CaioEPS <caioergos@gmail.com>
2015-09-19 00:51:37 -0300

    Applications added to jobs

--- Gemfile
+++ Gemfile
@@ -14,8 +14,9 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+# Good console visualization
+gem 'hirb'
 # Materialize-sass
-
 gem 'materialize-sass'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912143014) do
+ActiveRecord::Schema.define(version: 20150918211936) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "reasons"
+    t.integer  "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "applications", ["job_id"], name: "index_applications_on_job_id"
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
